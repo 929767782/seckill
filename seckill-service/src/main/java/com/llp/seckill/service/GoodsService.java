40ef6501d96e4222b1cd02c1dded41b1bbd515d7
llp <929767782@qq.com>
2022-07-31 00:43:58 +0800

    商品模块更新

--- seckill-service/src/main/java/com/llp/seckill/service/GoodsService.java
+++ seckill-service/src/main/java/com/llp/seckill/service/GoodsService.java
@@ -1,9 +1,8 @@
 package com.llp.seckill.service;
 
 
-import com.llp.seckill.entiry.SeckillGoods;
-import com.llp.seckill.vo.GoodsVo;
 import com.llp.seckill.mapper.GoodsMapper;
+import com.llp.seckill.vo.GoodsVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,8 +14,6 @@ import java.util.List;
 @Service
 public class GoodsService {
 
-    //乐观锁冲突最大重试次数
-    private static final int DEFAULT_MAX_RETRIES = 5;
 
     @Autowired
     GoodsMapper goodsMapper;
@@ -45,23 +42,7 @@ public class GoodsService {
      * @return
      */
     public boolean reduceStock(GoodsVo goods) {
-        int numAttempts = 0;
-        int ret = 0;
-        SeckillGoods sg = new SeckillGoods();
-        sg.setGoodsId(goods.getId());
-        sg.setVersion(goods.getVersion());
-        do {
-            numAttempts++;
-            try {
-                sg.setVersion(goodsMapper.getVersionByGoodsId(goods.getId()));
-                ret = goodsMapper.reduceStockByVersion(sg);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            if (ret != 0)
-                break;
-        } while (numAttempts < DEFAULT_MAX_RETRIES);
-
+        int ret = goodsMapper.reduceStockByVersion(goods.getId());
         return ret > 0;
     }
 }
